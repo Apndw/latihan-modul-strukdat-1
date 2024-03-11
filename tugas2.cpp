5f67ca3dafa958d3c9d607334e350b46aa1f2009
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-03-11 12:11:04 +0700

    chore(file): add code documentation

--- tugas2.cpp
+++ tugas2.cpp
@@ -2,14 +2,30 @@
 
 using namespace std;
 
+/**
+ * Membuat class Car untuk mengatur data kendaraan
+ *
+ * @class Car
+ */
 class Car {
+  // Inisiasi variable topspeed dengan default value 0
   double topspeed = 0;
 
   public:
-    string name, engine;
+    // Inisiasi variable untuk menyimpan nama dan tipe mesin kendaraan
+    char name[100], engine[100];
+
+    // Inisiasi variable untuk menyimpan data kecepatan kendaraan
     double speed = 0;
+
+    // Inisiasi variable untuk menyimpan data total gigi kendaraan
     int gear = 3;
     
+    /**
+     * Prosedural untuk mencetak tiap nilai dari public maupun private variable
+     *
+     * @return void
+     */
     void print() {
       topspeed = speed * gear;
       
@@ -23,22 +39,39 @@ class Car {
     }
 };
 
+/**
+ * Membuat struktur Driver untuk menyimpan data pembalap
+ *
+ * @struct Driver
+ */
 struct Driver {
   string nama, team;
   int number;
 };
 
 int main() {
+  // Inisiasi variable car dengan tipe data class Car
   Car car;
-  car.name = "RB20";
-  car.engine = "V6";
+
+  // Isi data nama dan mesin dengan fungsi strcpy
+  // Digunakan untuk mengubah string ke tipe data char
+  strcpy(car.name, "RB20");
+  strcpy(car.engine, "V6");
+
+  // Isi nilai dengan integer
   car.gear = 8;
   car.speed = 45;
+
+  // Cetak data kendaraan
   car.print();
 
+  //Inisiasi variable driver dengan tipe data struct Driver
   Driver driver;
+
+  // Isi nilai struct
   driver = { "Max Verstappen", "Red Bull Power Train Honda", 1 };
 
+  // Cetak nilai dari struct pada variable driver
   cout << "Nama pembalap " << driver.nama << endl;
   cout << "Team pembalap " << driver.team << endl;
   cout << "Nomor pembalap " << driver.number << endl;
